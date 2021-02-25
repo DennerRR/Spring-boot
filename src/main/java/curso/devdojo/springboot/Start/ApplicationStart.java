3c4e394c50e4506ee994fa4f14e98ae226df7da9
Denner Ramiro Ribeiro <dennerramiror@gmail.com>
2021-02-25 16:34:23 -0300

    Changing Version to 2.3.4 e resolving dependencies issues.

---
+++ src/main/java/curso/devdojo/springboot/Start/ApplicationStart.java
@@ -0,0 +1,12 @@
+package curso.devdojo.springboot.Start;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class ApplicationStart {
+    public static void main(String[] args){
+        SpringApplication.run(ApplicationStart.class);
+
+
+    }
+}
---
+++ src/main/java/curso/devdojo/springboot/domain/Anime.java
@@ -0,0 +1,12 @@
+package curso.devdojo.springboot.domain;
+
+public class Anime {
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+    public Anime(){
+
+    }
+}
